f245fa9933e1c26047c2ab3c7d862121d1f4d1e0
*Oskari Läntinen <oskari.lantinen@louhosdigital.fi>
2022-10-31 16:53:59 +0200

    init b

---
+++ src/lib/contentfulClient.js
@@ -0,0 +1,6 @@
+import contentful from 'contentful';
+
+export const client = contentful.createClient({
+    space: "az1alibwo2k8",
+    accessToken: "6__kSivEV-AK-n5o6jDO9hB21Tc397LG7OJ45k5sK7s"
+});
---
+++ src/routes/cont/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let data;
+    let org = data.body.org.items[0].fields.data;
+    console.log(org);
+
+</script>
+
+
+
+<div class="row">
+    {#each org as group}
+    <h2>{group.tableName}</h2>
+        <div class="column">
+        </div>
+    {/each}
+
+</div>
