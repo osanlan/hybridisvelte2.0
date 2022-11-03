779519d4ba7a069fe303c001757a3df5d7cb1792
*Oskari Läntinen <oskari.lantinen@louhosdigital.fi>
2022-11-03 12:19:11 +0200

    org

--- src/routes/yhteystiedot/+page.svelte
+++ src/routes/yhteystiedot/+page.svelte
@@ -1,17 +1,62 @@
 <script lang="ts">
     export let data;
     let org = data.body.org.items[0].fields.data;
-    console.log(org)
+    import Fa from 'svelte-fa/src/fa.svelte';
+    import { faExternalLink } from '@fortawesome/free-solid-svg-icons'
 </script>
-<h2>HALLITUS</h2>
-<table class="row">
+<section class="row">
     {#each org as group}
     <h2>{group.tableName}</h2>
-        {#each group.people as person}
-        <tr>
-            <td>{person.name}</td>
-            <td>{person.title}</td>
-        </tr>
-        {/each}
+    <ul>
+    {#each group.people as person}
+        {#if (group.id == 'yhdenvertaiset' && person.link)}
+        <li>
+            <span class="text">{person.text} <a href="{person.link}" target="_blank">linkki lomakkeeseen</a> <Fa icon={faExternalLink} /></span>
+        </li>        
+        {:else}
+        <li>
+            <span>{person.name}</span>
+            <span>{person.title}</span>
+        </li>
+        {/if}
     {/each}
-</table>
+    </ul>
+    {/each}
+</section>
+
+<style lang="scss">
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        h2 {
+            text-align: center;
+        }
+        ul {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            right: 0;
+            li {
+                list-style: none;
+                display: flex;
+                i {
+                    color: red;
+                }
+                // flex-wrap: wrap;
+                span {
+                    width: 100%;
+                    max-width: 400px;
+                    padding: 0 10px;
+                    &:first-child {
+                        text-align: right;
+                    }
+                    &.text {
+                        text-align: center;
+                    }
+                }
+            }
+        }
+    }
+</style>
