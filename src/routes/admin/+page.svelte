0db892c8db87d77b282376ad22d676eb19a28455
*Oskari Läntinen <oskari.lantinen@louhosdigital.fi>
2022-11-16 15:01:03 +0200

    init

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+    import { Canvas } from '@threlte/core';
+    import Scene from '$lib/components/Scene.svelte'
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import type { UserConfig } from 'vite';
 const config: UserConfig = {
 	plugins: [sveltekit()],
     ssr: {
-        noExternal: ['@popperjs/core']
+        noExternal: ['three', 'troika-three-text']
     }
 };
 
